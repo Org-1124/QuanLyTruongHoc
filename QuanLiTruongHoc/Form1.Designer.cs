6f4a47800bfa9d5eda819623aa14eec6cbdae946
dau-go <hosyviet95@gmail.com>
2016-09-29 14:28:05 +0700

    a

--- QuanLiTruongHoc/Form1.Designer.cs
+++ QuanLiTruongHoc/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.btngiaovien = new System.Windows.Forms.ToolStripMenuItem();
             this.btnlop = new System.Windows.Forms.ToolStripMenuItem();
             this.btnbomon = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,12 +55,14 @@
             this.btnhocsinh.Name = "btnhocsinh";
             this.btnhocsinh.Size = new System.Drawing.Size(67, 20);
             this.btnhocsinh.Text = "Học Sinh";
+            this.btnhocsinh.Click += new System.EventHandler(this.btnhocsinh_Click);
             // 
             // btngiaovien
             // 
             this.btngiaovien.Name = "btngiaovien";
             this.btngiaovien.Size = new System.Drawing.Size(69, 20);
             this.btngiaovien.Text = "Giáo Viên";
+            this.btngiaovien.Click += new System.EventHandler(this.btngiaovien_Click);
             // 
             // btnlop
             // 
@@ -73,11 +76,21 @@
             this.btnbomon.Size = new System.Drawing.Size(61, 20);
             this.btnbomon.Text = "Bộ Môn";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(951, 418);
+            this.tabControl1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 442);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -96,6 +109,7 @@
         private System.Windows.Forms.ToolStripMenuItem btngiaovien;
         private System.Windows.Forms.ToolStripMenuItem btnlop;
         private System.Windows.Forms.ToolStripMenuItem btnbomon;
+        private System.Windows.Forms.TabControl tabControl1;
 
     }
 }
--- QuanLiTruongHoc/Form1.cs
+++ QuanLiTruongHoc/Form1.cs
@@ -12,9 +12,38 @@ namespace QuanLiTruongHoc
 {
     public partial class Form1 : Form
     {
+        TabPage tabPage1 = new TabPage();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnhocsinh_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabPage1);
+            Form2 f = new Form2();
+            f.TopLevel = false;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.Dock = DockStyle.Fill;
+            
+            tabPage1.Controls.Add(f);
+            f.Visible = true;
+            tabPage1.Text = "Học Sinh";
+            tabControl1.TabPages.Add(tabPage1);
+        }
+
+        private void btngiaovien_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabPage1);
+            Form2 f = new Form2();
+            f.TopLevel = false;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.Dock = DockStyle.Fill;
+
+            tabPage1.Controls.Add(f);
+            f.Visible = true;
+            tabPage1.Text = "Giao Viên";
+            tabControl1.TabPages.Add(tabPage1);
         }
     }
 }
